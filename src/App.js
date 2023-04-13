270f5b04205054316aea898dc3e0e0e602c4384f
nico cappai <nicocappai@yahoo.it>
2023-04-13 18:18:05 +0200

    init project react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './components/Navbar.js';
 
-export default App;
+export default function App() {
+  
+  let message = 'Benvenuti al corso di React';
+  let names = ['Valerio', 'Nico', 'Giovanna'];
+  let lessons = [
+    { id: 1, title: 'lezione 1' , teacher: 'Valerio'},
+    { id: 1, title: 'lezione 2' , teacher: 'Roberto'},
+    { id: 1, title: 'lezione 3' , teacher: 'Laura'}
+  ]
+  
+  return (
+
+
+    <Navbar />
+    // <>
+    
+    //   <h1 classNameNameName='titolo'>Welcome</h1>
+    //   <p>{2 + 2}</p>
+    //   <p> {('') ? 4 : NaN}</p>
+    //   <p>{message}</p>
+
+    //   {lessons.map((lesson) => {
+
+    //     return (
+
+    //       <div key={lesson.id}>
+    //         <h2> {lesson.title} </h2>
+    //         <p> Docente: {lesson.teacher} </p>
+    //       </div>
+    //     );
+
+    //   })}
+
+    //   /*
+    //   <input type="" />
+    //   <img src=''> </img>
+    //   */
+    // </>
+    
+    
+    
+    );
+  }
+  
+   // Tag speciale -> <React.Fragment>
